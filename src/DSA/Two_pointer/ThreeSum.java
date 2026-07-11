5aa472aa2c5c1fa6c37f1257aa7942b6d426a8f1
Neerajjangir2001 <neerajjangir2001@gmail.com>
2026-07-11 13:16:16 +0530

    problem number : 15 3Sum

---
+++ src/DSA/Two_pointer/ThreeSum.java
@@ -0,0 +1,70 @@
+package DSA.Two_pointer;
+
+
+//15. 3Sum
+//
+//Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+//
+//Notice that the solution set must not contain duplicate triplets.
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class ThreeSum {
+
+    public List<List<Integer>> threeSum(int[] nums) {
+
+        Arrays.sort(nums);
+        List<List<Integer>> list = new ArrayList<>();
+
+
+        for (int i = 0; i < nums.length - 2; i++) {
+
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+            int left = i + 1;
+            int right = nums.length - 1;
+
+            while (left < right) {
+
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (sum == 0) {
+
+                    list.add(Arrays.asList(nums[i], nums[left], nums[right]));
+
+                    while (left < right && nums[left] == nums[left + 1]) left++;
+                    while (left < right && nums[right] == nums[right - 1]) right--;
+
+                    left++;
+                    right--;
+
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    right--;
+                }
+
+            }
+        }
+
+
+return list;
+
+
+    }
+
+    public static void main(String[] args) {
+
+        ThreeSum solution = new ThreeSum();
+
+        int[] nums = {-1, 0, 1, 2, -1, -4};
+
+        List<List<Integer>> result = solution.threeSum(nums);
+
+        System.out.println(result);
+    }
+}
